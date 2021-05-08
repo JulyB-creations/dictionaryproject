890acaa2840998d6b4642fc4fae04f5d298472e2
JulyB-creations <79759706+JulyB-creations@users.noreply.github.com>
2021-05-08 19:37:06 +0200

    added search form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 
 import logo from "./Logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo"/>
-
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Coded by J.B
+      </footer>
+      </div>
     </div>
   );
 }
